ed078d606acef77ca733a2de747fdb2fa0804c1c
Milton Chang <mchangpi@gmail.com>
2020-11-10 13:01:22 +0800

    Style: modify landing page font size

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,14 +3,14 @@ import React from "react";
 const Landing = () => {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Milton Survey Mailer</h1>
-      <h5>Collect feedback from your users</h5>
-      <h5>
+      <h2>Milton Survey Mailer</h2>
+      <h6>Collect feedback from your users</h6>
+      <h6>
         Please also check my{" "}
         <a href="https://www.linkedin.com/in/milton-chang-pi/" target="_blank">
           LinkedIn
         </a>
-      </h5>
+      </h6>
     </div>
   );
 };
